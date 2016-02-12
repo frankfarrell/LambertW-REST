3b2f30f90c1fea7e9f76d12d63dec4c5113886e4
frankfarrell <FrankFa@bluetreesystems.com>
2016-02-12 10:27:24 +0000

    Added travis intergration

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,7 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 import java.util.Arrays;
 
 /**
---
+++ src/test/java/PlaceholderTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by FrankFa on 12/02/2016.
+ */
+public class PlaceholderTest {
+
+
+    @Test
+    public void mockTest(){
+        assert(1+1 == 2);
+    }
+
+}
